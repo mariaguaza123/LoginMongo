026a1783806a720fd24a9df03b6e0d79f2bdcc0d
mariaguaza123 <43611721+mariaguaza123@users.noreply.github.com>
2023-03-29 12:53:53 -0500

    Se actualiza el puerto

--- Login/LoginAndNormalizr/index.js
+++ Login/LoginAndNormalizr/index.js
@@ -1,5 +1,5 @@
 const server = require('./src/services/server');
-const puerto = 8080;
+const port = process.env.PORT || 3001;
 
 server.listen(puerto, () =>{
 console.log(`Servidor listo escuchando en el puerto ${puerto}`);
